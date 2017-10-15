61844ceb56c15aad91cf596530663616c14a9c7a
Antoine <antoine.cadotte@gmail.com>
2017-10-15 08:03:33 -0400

    Fixed major issues with payment.

--- contracts/Shippable.sol
+++ contracts/Shippable.sol
@@ -5,9 +5,11 @@ contract Shippable {
 
     mapping (address => uint) balances;
 
-    uint productValue;
-    uint deposit;
-    uint initialShipPrice;
+    function () payable {}
+
+    uint public productValue;
+    uint public deposit;
+    uint public initialShipPrice;
 
     struct TransportInfo {
 	    string deliveryLocation;
@@ -48,7 +50,7 @@ contract Shippable {
     function Shippable(uint _productValue, string _currentLocation) public payable {
     	owner = msg.sender;
     	seller = owner;
-    	productValue = _productValue;
+    	productValue = _productValue*(10**18);
     	transportInfo.currentLocation = _currentLocation;
     	sellingState = SellingState.ForSale;
     }
@@ -56,14 +58,16 @@ contract Shippable {
     // Confirms that the buyer wants to purchase the item at its productValue and ship it at deliveryLocation
     // for shipPrice before _shippingTime seconds. Transfers the productValue and shipPrice on the contract.
     function buy(string _deliveryLocation, uint shipPrice, uint _deadline) public payable {
-    	if ((msg.value >= (productValue + shipPrice)*10^18) && sellingState == SellingState.ForSale){
+    	if ((msg.value >= productValue + shipPrice*(10**18)) && sellingState == SellingState.ForSale){
         	transportInfo.deliveryLocation = _deliveryLocation;
-        	initialShipPrice = shipPrice;
+        	initialShipPrice = shipPrice*(10**18);
     	    transportInfo.currentShipPrice = initialShipPrice;
     	    transportInfo.deadline = _deadline;
         	buyer = msg.sender;
+        	this.transfer(productValue + shipPrice);
         	transferOwnership(buyer);
         	sellingState = SellingState.Sold;
+        	deposit = productValue;
     	}
 	}
 
@@ -75,15 +79,16 @@ contract Shippable {
 	// Accept to take responsibility of the shipped item
     function acceptTransfer() public payable {
 		if(shippingState == ShippingState.Awaiting){
-			if (msg.value >= deposit*10^18){
+			if (msg.value >= deposit){
+				seller.transfer(productValue);
 				newShipper(msg.sender, transportInfo.currentShipPrice);
 				currentShipper = msg.sender;
 				shippingState = ShippingState.Shipping;
 			}
 		}
 		else if (shippingState == ShippingState.Shipping){
-			if (msg.value >= deposit*10^18){
-		        currentShipper.transfer(deposit);
+			if (msg.value >= deposit){
+		        currentShipper.transfer(productValue);
 		        newShipper(msg.sender, transportInfo.currentShipPrice);
 		        currentShipper = msg.sender;
 			}
@@ -92,16 +97,24 @@ contract Shippable {
 
     // Offer to transfer shipped item from current shipper to new shipper
     function transferItem(uint newShipPrice, string _currentLocation) public {
-    	if (msg.sender == currentShipper || msg.sender == seller){
-    		transportInfo.currentShipPrice = newShipPrice;
+    	if (msg.sender == currentShipper){
+    		transportInfo.currentShipPrice = newShipPrice*(10**18);
     		transportInfo.currentLocation = _currentLocation;
-    		deposit = productValue;
     	}
     }
 
     // Verifies if the current shipper is responsible for the shipped item
-    function isMyItem() public returns (bool) {
+    function isMyShipment() public returns (bool) {
     	if(msg.sender == currentShipper){
+    		return true;
+    	}
+
+    	else return false;
+    }
+    
+    // Verifies if the msg.sender is owner of the shipped item
+    function isMyItem() public returns (bool) {
+    	if(msg.sender == owner){
     		return true;
     	}
 
@@ -118,20 +131,23 @@ contract Shippable {
     }
 
     // Confirms that the item was delivered to the owner/buyer
-    function delivered() public {
+    function delivered() payable public {
     	if (msg.sender == owner){
 	    	// Pay shipping price to all shippers
 	    	uint shipperID = numShippers-1;
-	    	shippers[shipperID].addr.transfer(shippers[shipperID].shipPrice);
-	    	for (shipperID=numShippers-1; shipperID>=0; shipperID--){
-	    		shippers[shipperID].addr.transfer(shippers[shipperID].shipPrice - shippers[shipperID+1].shipPrice);
-	    	}
+	    	address shipper = shippers[shipperID].addr;
+	    	uint shipFee = shippers[shipperID].shipPrice + deposit;
+	    	shipper.transfer(shipFee);
+	    	shipperID=0;
+    	    shipper = shippers[shipperID].addr;
+    	    shipFee = shippers[shipperID].shipPrice - shippers[shipperID+1].shipPrice;
+    	    shipper.transfer(shipFee);
 	    	shippingState = ShippingState.Delivered;
     	}
     }
 
     // Refund owner if deadline is past
-    function checkDeadline() public {
+    function checkDeadline() payable public {
     	if (now > transportInfo.deadline){
 			owner.transfer(deposit + initialShipPrice);
     	}
