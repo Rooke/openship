35c9341a23f75a77f7652e079fb5c2ea41a1a5eb
Oasis Vali <oasis.vali@gmail.com>
2017-10-15 02:26:30 -0400

    cleanup

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,3 +3,5 @@ import React from 'react';
 const Welcome = () => (
   <h1>Choose Function</h1>
 );
+
+export default Welcome;
--- src/reducers/items.js
+++ src/reducers/items.js
@@ -1,3 +1,21 @@
+const ITEMS_DATA = [
+  {
+    title: 'yellow bike',
+    src: 'https://i.pinimg.com/736x/40/ba/c8/40bac89fdf296d77e22abe228739d87f--art-students-candy-land.jpg',
+    description: 'Travel wit style and comfort',
+  },
+  {
+    title: 'unicorn bike',
+    src: 'https://i.pinimg.com/736x/54/af/51/54af512f77758d98397e3497d391c107--unicorn-bike-unicorn-mask.jpg',
+    description: 'Travel wit more style and comfort',
+  },
+  {
+    title: 'banana',
+    src: 'https://i5.walmartimages.ca/images/Enlarge/580/6_r/875806_R.jpg',
+    description: 'helps you become less hungry',
+  }
+];
+
 const initialState = {
   items: []
 }
@@ -6,7 +24,11 @@ function items(state = initialState, action) {
   switch(action.type) {
     case 'SET_ITEM_INFO':
       const items = [ ...state.items ];
-      items[action.itemIndex] = { ...items[action.itemIndex], ...action.itemInfo };
+      items[action.itemIndex] = {
+        ...ITEMS_DATA[action.itemIndex % 3],
+        ...items[action.itemIndex],
+        ...action.itemInfo
+      };
       return { items };
     default:
       return state;
